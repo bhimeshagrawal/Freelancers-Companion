be390339b8e128015faa59ad128ed489d312fd6b
bhimeshagrawal <bhimeshagrawalggc@gmail.com>
2021-10-24 00:56:34 +0530

    corrected app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.PORT || 5000;
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
-app.listen(3000,function(){
-    console.log("Server Started at port 3000");
+app.listen(PORT,function(){
+    console.log("Server Started at port 5000");
 })
 
 app.get("/",function(req,res){
