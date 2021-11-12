b926f44538331192bc44e06b9c77538cf43868e1
bhimeshagrawal <bhimeshagrawalggc@gmail.com>
2021-11-12 17:18:23 +0530

    added trello functionality

--- app.js
+++ app.js
@@ -1,108 +1,81 @@
-var express = require("express");
-var app = express();
-var passport = require("passport")
+//installing the packages
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    passport       = require("passport"),
+    cookieParser   = require("cookie-parser"),
+    LocalStrategy  = require("passport-local"),
+    flash          = require("connect-flash"),
+    session        = require("express-session"),
+    methodOverride = require("method-override"),
+    request        = require("request"),
+    apiKey         = "f1e0f999122f692de81fc8ede3be89a0",
+    apiToken       = "c8a5a4506b6496da9691b124497869213f027d9ad4d8d89cbee079cc3edee318",
+    boardId        = "5fcd8e3b73ed780e1b520d51",
+    pendingListId  = "618e3cadf3c7b72e9b5438ca",
+    activeListId   = "618e4d1b292998103df01f9b";
+
+
+//configurations
 const PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+app.use(cookieParser('secret'));
 
-app.set("view engine","ejs");
-app.use(express.static("public"));
 
+// routes
 app.listen(PORT,function(){
     console.log(`Server Started at port ${PORT}`);
 })
-
 app.get("/",function(req,res){
     res.render("home");
 })
-
 app.get("/login",function(req,res){
     res.render("login")
 })
-
 app.get("/signup",function(req,res){
     res.render("signup")
 })
 
-var projectList = [
-    {
-        _id:"1",
-        title : "Captain Americal Artwork",
-        image : "https://source.unsplash.com/random/",
-        completedDate: "",
-        orderedDate:"11/11/11",
-        category : "Artwork",
-        estimatedDate : "-",
-        status : "pending", 
-    },{
-        _id:"",
-        title : "Captain Americal Logo",
-        image : "https://source.unsplash.com/random/",
-        completedDate: "",
-        orderedDate:"20/11/11",
-        category : "Logo",
-        estimatedDate : "12/10/2021",
-        status : "active",
-    },
-    {
-        _id:"",
-        title : "Captain Americal Poster",
-        image : "https://source.unsplash.com/random/",
-        completedDate: "13/12/21",
-        orderedDate:"11/11/11",
-        category : "Poster",
-        estimatedDate : "12/10/2021",
-        status : "completed", 
-    },{
-        _id:"1",
-        title : "Captain Americal Artwork",
-        image : "https://source.unsplash.com/random/",
-        completedDate: "",
-        orderedDate:"11/11/11",
-        category : "Artwork",
-        estimatedDate : "-",
-        status : "pending", 
-    },
-    {
-        _id:"1",
-        title : "Captain Americal Artwork",
-        image : "https://source.unsplash.com/random/",
-        completedDate: "",
-        orderedDate:"11/11/11",
-        category : "Artwork",
-        estimatedDate : "-",
-        status : "pending", 
-    },
-    {
-        _id:"1",
-        title : "Captain Americal Artwork",
-        image : "https://source.unsplash.com/random/",
-        completedDate: "",
-        orderedDate:"11/11/11",
-        category : "Artwork",
-        estimatedDate : "-",
-        status : "pending", 
-    },
-    {
-        _id:"1",
-        title : "Captain Americal Artwork",
-        image : "https://source.unsplash.com/random/",
-        completedDate: "",
-        orderedDate:"11/11/11",
-        category : "Artwork",
-        estimatedDate : "-",
-        status : "pending", 
-    },
-    {
-        _id:"1",
-        title : "Captain Americal Artwork",
-        image : "https://source.unsplash.com/random/",
-        completedDate: "",
-        orderedDate:"11/11/11",
-        category : "Artwork",
-        estimatedDate : "-",
-        status : "pending", 
-    }
-]
-app.get("/app",function(req,res){
-    res.render("app",{projectList : projectList});
+// list all cards 
+app.get("/projects",function(req,res){
+    var projectList = [];
+    var url = `https://api.trello.com/1/boards/5fcd8e3b73ed780e1b520d51/cards?key=f1e0f999122f692de81fc8ede3be89a0&token=c8a5a4506b6496da9691b124497869213f027d9ad4d8d89cbee079cc3edee318`
+    request.get(url, function (err, response, body) {
+        if (!err)
+        {
+             projectList = JSON.parse(body);
+             console.log(projectList);
+        }
+        else
+            console.log(err)
+    })
+    res.render("projects",{projectList : projectList});
 })
+
+// create a new card 
+app.post("/projects", function(req, res){
+    // get data from form and add to projectList array
+    var projectTitle = req.body.projectTitle;
+    var category = req.body.category;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+    var newProject = {
+        title: projectTitle,  
+        category: category, 
+        orderedDate: today,
+        estimatedDate: "",
+        completedDate: ""
+    }
+    var postApiLink = `https://api.trello.com/1/cards?key=${apiKey}&token=${apiToken}&idList=${pendingListId}&name=${projectTitle}&desc=${category}`
+    console.log(postApiLink)
+    request.post(postApiLink);
+    res.redirect("/projects");
+});
+
 
